613b93b3b78000387cb570ce58cc830c56dbdf39
Qudus Lawal <lawalqudus980@gmail.com>
2023-12-12 14:12:41 +0100

    Create react components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import App from './app/App';
 import reportWebVitals from './test/reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>
 );
 
